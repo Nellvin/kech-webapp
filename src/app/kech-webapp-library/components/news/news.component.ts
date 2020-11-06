9e8bc0b7fd1c1119561217168c86442a1f4c192a
Filip <floffler@wp.pl>
2020-11-06 17:54:56 +0100

    news component first creation

--- src/app/kech-webapp-library/components/news/news.component.ts
+++ src/app/kech-webapp-library/components/news/news.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  @Input()
+  news: any;
+  title : string;
+  content : string;
+  formatedDate : string;
+  author : string;  
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formatCreateDate(this.news.createDate)
+  }
+
+  formatCreateDate(date: Date){
+    this.formatedDate = formatDate(date,"dd\nMM\n\nyy","en-EN");
   }
 
 }
