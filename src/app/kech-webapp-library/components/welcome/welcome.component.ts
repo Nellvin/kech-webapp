a499e8de23d57c4e4d4619527848ab69a9b8fd15
Filip <floffler@wp.pl>
2020-11-07 01:17:18 +0100

    welcome page adapt

--- src/app/kech-webapp-library/components/welcome/welcome.component.ts
+++ src/app/kech-webapp-library/components/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private newsService : NewsService,
+              private sermonService : SharedService) { }
+
+  newsList: any= [];
+  sermonsList: any= [];
 
   ngOnInit(): void {
+    this.refreshNewsList();
+    this.refreshSermonsList();
+  }
+
+  refreshNewsList(){
+    this.newsService.getNewiestNews().subscribe(data =>{
+      this.newsList = data;
+      this.newsList.map( news => {
+        news.content = news.content.substring(0,200)+"...";
+        // news.createDate = news.createDate.substring(0,10);
+      })
+    })
+  }
+  
+  refreshSermonsList(){
+    this.sermonService.getNewiestSermons().subscribe(data =>{
+      this.sermonsList = data;
+    })
   }
 
 }
