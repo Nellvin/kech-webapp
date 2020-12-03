9b0b0826cee6648acc62d370d9f8368a6623712b
Filip <floffler@wp.pl>
2020-12-03 13:41:54 +0100

    added youtube tab

---
+++ src/app/kech-webapp-library/components/youtube-live/youtube-live.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { YoutubeLiveComponent } from './youtube-live.component';
+
+describe('YoutubeLiveComponent', () => {
+  let component: YoutubeLiveComponent;
+  let fixture: ComponentFixture<YoutubeLiveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ YoutubeLiveComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(YoutubeLiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/kech-webapp-library/components/youtube-live/youtube-live.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-live',
+  templateUrl: './youtube-live.component.html',
+  styleUrls: ['./youtube-live.component.scss']
+})
+export class YoutubeLiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
