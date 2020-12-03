991d4d6d7d7df73f17f92b91f14e65f9debe4fce
Filip <floffler@wp.pl>
2020-12-03 13:48:45 +0100

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { SeremonsComponent } from './kech-webapp-library/components/seremons/ser
 import { AboutUsComponent } from './kech-webapp-library/components/about-us/about-us.component';
 import { GalleryComponent } from './kech-webapp-library/components/gallery/gallery.component';
 import { NewsPageComponent } from './kech-webapp-library/components/news-page/news-page.component';
+import { YoutubeLiveComponent } from './kech-webapp-library/components/youtube-live/youtube-live.component';
+import { LoginComponent } from './kech-webapp-library/components/Administration/login/login.component';
+import { AdminComponent } from './kech-webapp-library/components/Administration/admin/admin.component';
+import { AuthGuard } from './Guard/auth.guard';
 
 const routes: Routes = [
   {path: 'home', component: WelcomeComponent},
@@ -12,6 +16,9 @@ const routes: Routes = [
   {path: 'kontakt', component: AboutUsComponent},
   {path: 'aktualnosci', component: NewsPageComponent},
   {path: 'galeria', component: GalleryComponent}, 
+  {path: 'live', component: YoutubeLiveComponent}, 
+  {path: 'login', component: LoginComponent}, 
+  {path: 'admin', component: AdminComponent, canActivate: [AuthGuard]}, 
   {path: '',   redirectTo: '/home', pathMatch: 'full' },
   // {path: '**', component: WelcomeComponent},
 ];
